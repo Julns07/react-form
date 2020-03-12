2e5f83c9a48260b33d0514e3afc460a86afb059a
Julio Alonso <jalonso4078@gmail.com>
2020-03-12 15:45:18 -0700

    final

---
+++ src/components/Thanks.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default props => {
+  return (
+    <div className="thanks">
+      <h1>Thank You!</h1>
+      <h3>Profile has been updated</h3>
+    </div>
+  )
+}
